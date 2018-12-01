7d58a27a74008f8bb4dbb6f8985b15331a80b951
Kevin Mees <kev.mees@gmail.com>
2018-12-01 23:54:06 +0100

    smaller content width

--- app/src/components/Layout.tsx
+++ app/src/components/Layout.tsx
@@ -2,7 +2,7 @@ import styled, { Flex } from '../styled'
 
 export const ContentContainer = styled(Flex)`
   width: 100%;
-  max-width: 1024px;
+  max-width: 768px;
   margin: 0 auto;
   overflow-y: auto;
 `
