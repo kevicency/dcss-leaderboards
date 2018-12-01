0b727cd17b22cf65b46326b7ee3f869c228567d0
Kevin Mees <kev.mees@gmail.com>
2018-12-01 23:46:19 +0100

    update video urls in rankings

--- functions/src/jobs/video.ts
+++ functions/src/jobs/video.ts
@@ -1,6 +1,12 @@
 import { videos } from '../data'
 import { createLogger } from '../logger'
-import { Speedrun } from '../model'
+import {
+  RankingByBackground,
+  RankingByGod,
+  RankingByPlayer,
+  RankingByRace,
+  Speedrun,
+} from '../model'
 import { Job } from './job'
 
 const logger = createLogger('video')
@@ -12,7 +18,27 @@ export class SpeedrunVideoUpdateJob implements Job {
     for (const gid in videos) {
       logger.info(`updating vod of ${gid}`)
 
-      await Speedrun.updateOne({ gid }, { vod: videos[gid] })
+      await Speedrun.updateOne({ gid }, { vod: videos[gid] }, { upsert: false })
+      await RankingByBackground.updateOne(
+        { gid },
+        { vod: videos[gid] },
+        { upsert: false }
+      )
+      await RankingByGod.updateOne(
+        { gid },
+        { vod: videos[gid] },
+        { upsert: false }
+      )
+      await RankingByPlayer.updateOne(
+        { gid },
+        { vod: videos[gid] },
+        { upsert: false }
+      )
+      await RankingByRace.updateOne(
+        { gid },
+        { vod: videos[gid] },
+        { upsert: false }
+      )
     }
 
     logger.debug('job done.')
