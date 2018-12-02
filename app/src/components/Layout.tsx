7a7fbf38dfd3e4f49f7ecd38dde0ad84153852ee
Kevin Mees <kev.mees@gmail.com>
2018-12-02 17:20:07 +0100

    increase content width to 800px

--- app/src/components/Layout.tsx
+++ app/src/components/Layout.tsx
@@ -2,7 +2,7 @@ import styled, { Flex } from '../styled'
 
 export const ContentContainer = styled(Flex)`
   width: 100%;
-  max-width: 768px;
+  max-width: 800px;
   margin: 0 auto;
   overflow-y: auto;
 `
